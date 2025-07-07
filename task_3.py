32c2fa1a1459e0d8f9376a3164e531872bec1a32
Dmitrii Gubanov <dmltrlll7111@gmail.com>
2025-07-07 18:46:57 +0300

    task_3 fix

--- task_3.py
+++ task_3.py
@@ -1,40 +1,30 @@
-
+points = 0
 
 class PointsForPlace:
-    points = 0
     @staticmethod
     def get_points_for_place(place):
-    
         if place > 100:
             print('Баллы начисляются только первым 100 участникам')
         elif place < 1:
             print('Спортсмен не может занять нулевое или отрицательное место')
         else:
             points = 101 - place
-
         return points
     
 class PointsForMeters:
-    points = 0
     @staticmethod
     def get_points_for_meters(meters):
-    
         if meters < 0:
            print("Количество метров не может быть отрицательным")
         else:
             points = meters * 0.5
-
         return points
     
 class TotalPoints(PointsForPlace, PointsForMeters):
-
-    @staticmethod
-    def get_total_points(meters, place):
-        place_points = PointsForPlace.get_points_for_place(place)
-        meters_points = PointsForMeters.get_points_for_meters(meters)
-        total = place_points + meters_points
-
-        return total
+    def get_total_points(self, meters, place):
+        place_points = self.get_points_for_place(place)
+        meters_points = self.get_points_for_meters(meters)
+        return place_points + meters_points
 
 points_for_place = PointsForPlace()
 print(points_for_place.get_points_for_place(10))
@@ -45,4 +35,4 @@ print(points_for_meters.get_points_for_meters(10))
 total_points = TotalPoints()
 print(total_points.get_points_for_place(10))
 print(total_points.get_points_for_meters(10))
-print(total_points.get_total_points(100, 10)) 
+print(total_points.get_total_points(100, 10))
